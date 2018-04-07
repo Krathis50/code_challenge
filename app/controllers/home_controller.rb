f4523085cbb0d448d5ee91a4a1e1f5cacdce4551
Ashish Rao <ashishrao2598@gmail.com>
2018-04-07 20:02:36 +0530

    error messages upon submitting of empty field

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,11 @@ class HomeController < ApplicationController
   def submit
     code = params[:rubyCode]
     result = eval(code)
-    redirect_to root_url, notice: "The result of the code is #{ result }"
+    if result.instance_eval(code)
+      redirect_to root_url, notice: "The result of the code is #{ result }"
+    else
+      redirect_to root_url, notice: "Cannot be left blank or error in code #{code}"
+    end
   end
+  
 end
