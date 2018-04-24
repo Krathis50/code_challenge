fcaeb4e0fe4dee380217a7d57d78c42f9926a4d2
Alex Kitchens <alex.kitchens@stitchfix.com>
2018-04-23 22:06:26 -0500

    Evaluate based off of args

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :problem_sets, only: [:new, :edit, :create, :destroy]
-  resources :challenges
+  resources :challenges do
+    post "submit" => "challenges#submit"
+  end
   devise_for :users
   root "home#index"
 
