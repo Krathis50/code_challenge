2a89bcf3b8e5bb2a5bd5a17c9139c0f5c4dab9b9
Petterson Pham <Wellwardedx10@gmail.com>
2018-04-24 19:51:07 -0500

    Added basic login and logout functions, and added the login and out at top right.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
   resources :problem_sets, only: [:new, :edit, :create, :destroy]
   resources :challenges
-  devise_for :users
-  root "home#index"
+  devise_for :user
 
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+    
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+
+    get  "login",   to: "devise/sessions#new"
+    post "login",   to: "devise/sessions#create"
+    get "logout",   to: "devise/sessions#destroy"
+  end
+
+  #root "home#index"
   resources :canvas
 
   get  "home/index"
   post "home/submit"
+
+
 end
