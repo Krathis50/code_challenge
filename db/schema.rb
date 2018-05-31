a87630d8e07da84c335d8f1fbf35e9ba3744c42b
Kyler Blodgett <kablodgett1@buffs.wtamu.edu>
2018-05-31 17:23:16 -0500

    added scoring fields and changes to scoreboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180502051155) do
-
+ActiveRecord::Schema.define(version: 20180531221736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +68,10 @@ ActiveRecord::Schema.define(version: 20180502051155) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.integer "role"
+    t.string "school"
+    t.integer "score"
+    t.string "team"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
