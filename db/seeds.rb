d0b95ad2743b213083c07ce865d025e6420015e3
Petterson Pham <Wellwardedx10@gmail.com>
2018-06-02 03:35:13 -0500

    Remoevd unnecessary line and added information on how to become admin through rails console.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #   how to make a a new user:
 #
+#   #DEPRECATE STARTS HERE
 #   user = User.create(:email => 'email here', :password => '123asd')   
 #   user.role = 1       ;assigns role of admin.
 #   user.save           ;saves the user's info and is ready to be logged in
@@ -17,4 +18,23 @@
 #   if you need to make an admin, do:
 #       user.role = 1
 #   which will set to admin, and 0 to set to default level user.
+#
+#   #DEPRECATE ENDS HERE
+#   
+#
+#   HOW TO MAKE YOURSELF ADMIN.
+#   Go to bash console, terminal, shell console or where you put linux commands in.
+#   type: rails c
+#   This should open up rails console and your commandline should look like:
+#   irb(main):numbers:numbers>
+#   type in User.find([number]) where number is an id.
+#   if you find your id, which the only way to distinguish whos who is by email,
+#   do the following
+#   User.update_attribute(:role, 1)
+#   it should update to postgresql after that.
+#   Congratulations, you're an admin!
+#
+#
+#
+#
 #
